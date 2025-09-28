a952f63dfb5e852d8303b03e2ce1ad0f0b88f686
AmirrezaMahmoudi <amirrezamh03@gmail.com>
2025-09-28 13:00:24 +0330

    fix: path

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,29 +2,32 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
-import cors from "cors"
-import { app , server} from "./lib/socket.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
 import path from "path";
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-  {
-    origin:"http://localhost:5173",
-    credentials: true
-  }
-));
-const PORT = process.env.PORT;
-const __dirname = path.resolve()
-if(process.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
   })
+);
+const PORT = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
