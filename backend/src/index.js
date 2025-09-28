f2dfd056694ce5d939ee0ba12eca46a32c2adbc0
AmirrezaMahmoudi <amirrezamh03@gmail.com>
2025-09-28 12:54:36 +0330

    fix: import

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
-import connectDB from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import cors from "cors"
 import { app , server} from "./lib/socket.js";
 import path from "path";
